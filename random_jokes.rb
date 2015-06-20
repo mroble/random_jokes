0059951eeae93552d5f591ac0adc9cef5c3a3045
mroble <mroble@users.noreply.github.com>
2015-06-19 20:33:53 -0400

    Add jokes to jokes array

---
+++ random_jokes.rb
@@ -0,0 +1,20 @@
+
+  def random_jokes
+    jokes = ["Knock knock. Who's there? Olive. Olive who? Olive you!", 
+      "What happens when you tell an egg a joke? It cracks up!", 
+      "What do you get when you cross a fish with an elephant? Swimming trunks!",
+      "What did the hat say to the scarf? You go around while go on ahead.",
+      "How do you warm up a room after it's been painted? Give it a second coat!",
+      "Where do snowflakes dance? At a Snow Ball!",
+      "Why did the tomato blush? Because it saw the salad dressing",
+      "Why did the farmer's wife chase the chickens out of the yard? They were using fowl language.",
+      "Why did the cowboy put his bunk in the fireplace? So he could sleep like a log.",
+      "What has a bottom at the top? Your legs!",
+      "What did one ghost say to the other ghost? Get a life!",
+      "Which weighs more? A ton of feathers or a ton of bricks? They both weigh a ton.",
+      "What can hold a car but can't lift a feather? A garage!"
+    ]
+    jokes.sample
+  end
+  
+  puts random_jokes
